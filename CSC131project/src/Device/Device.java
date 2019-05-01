1bf6c0f1dff580d6432b0713dd17a465a68c9852
Austin <austin1224@gmail.com>
2019-05-01 14:41:23 -0700

    device.java updated

--- CSC131project/src/Device/Device.java
+++ CSC131project/src/Device/Device.java
@@ -4,25 +4,47 @@ public class Device {
 	//this is the device driver
 	//most devices should have a name and an ID
 	private String name,ID;
-	public Device(String name, String ID) {
+	private String location;
+	private boolean lost;
+	public Device(String name, String ID,String location) {
 		this.name = name;
 		this.ID = ID;
+		this.location = location;
+		this.lost = false;
 	}
 	
 	
-	//setter methods
+	/**
+	 * setter methods
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 	public void setID(String ID) {
 		this.ID = ID;
 	}
+	public void setLocation(String location) {
+		this.location = location;
+	}
+	public void setLost(boolean lost) {
+		this.lost = lost;
+	}
 	
-	//getter methods
+	
+	
+	
+	/**
+	 * getter methods
+	 * @return
+	 */
 	public String getName() {
 		return this.name;
 	}
 	public String getID() {
 		return this.name;
+	}
+	public String getLocation() {
+		return this.location;
 	}
 }
