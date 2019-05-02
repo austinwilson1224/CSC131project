b0b362997f9c652f3ea7d330670a69a9a4792118
Austin <austin1224@gmail.com>
2019-05-02 11:47:47 -0700

    deleted main method in server

--- CSC131project/src/server/Server.java
+++ CSC131project/src/server/Server.java
@@ -3,18 +3,11 @@ package server;
 public class Server {
 	private int serverID;
 	
+	
 	public Server(int serverID) {
 		this.serverID = serverID;
 	}
 	
-
-	
-	public static void main(String[] args) {
-		Device Austin = new Device("Austin","1","Sacramento");
-		Austin.setLost();
-		System.out.println("You device is in " + Austin.getLocation());
-	}
-
 	
 	
 	public void setID(int id) {
