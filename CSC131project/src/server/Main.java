d62e6fd57fdd298af55f8df390ed74a43a761712
Jorge Israel Cedeno <Jorge Israel Cedeno@10.117.247.92>
2019-05-01 18:24:20 -0700

    Main class (:

---
+++ CSC131project/src/server/Main.java
@@ -0,0 +1,24 @@
+package server;
+
+public class Main {
+	public static void main(String[] args)
+	{
+		Verify(); 
+		
+	}
+	public static void Verify()
+	{
+		System.out.print("Please enter First name: ");
+		String Fname = scan.next();
+		System.out.println();
+		System.out.print("Please enter Last name: ");
+		String Lname = scan.next();
+		System.out.println();
+		System.out.print("Please enter Device ID: ");
+		String ID = scan.next();
+		System.out.println();
+	}
+	
+	
+
+}
