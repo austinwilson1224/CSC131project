e2c12177755a27beeaa6f99d23115fc65c1a51b4
CJ Angelina <cjtorres@csus.edu>
2019-05-04 00:41:04 -0700

    Add files via upload

---
+++ CSC131project/src/src/notification/Notification.java
@@ -0,0 +1,53 @@
+package notification;
+
+/*
+ * author CJ Angelina Torres
+ * 
+ */
+
+public class Notification {
+
+	private String description, id, timeStamp;
+	private String sender, receiver;
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getTimeStamp() {
+		return timeStamp;
+	}
+
+	public void setTimeStamp(String timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+
+	public String getSender() {
+		return sender;
+	}
+
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+
+	public String getReceiver() {
+		return receiver;
+	}
+
+	public void setReceiver(String receiver) {
+		this.receiver = receiver;
+	}
+
+}
---
+++ CSC131project/src/src/server/Server.java
@@ -0,0 +1,55 @@
+package server;
+
+public class Server {
+	
+	/**
+	 * this is where we need to code the listener method
+	 * 
+	 * there will be an instance of an object of maybe a user
+	 * when the person loses their device they need to notify the server
+	 * that the device is gone
+	 * the listener will listen for when this call goes out and will update 
+	 * the device status to show that it has been lost
+	 */
+	
+	
+	
+	//private User user;
+	//private Device device;
+	private int serverID;
+	
+	
+	/**
+	 *user notifies that the device has been lost){
+	  update device status
+	  also when the device is found then update to not lost....
+	 */
+	
+	  public void listener(){
+		  
+	  	//	if(user.getDevice().isLost()) {
+	  			
+	  			//finish later
+	  			//return user.getDevice().getLocation();
+	  		//}
+	  }
+
+	
+	
+	
+	public Server(int serverID) {
+		this.serverID = serverID;
+	}
+	
+	
+	
+	public void setID(int id) {
+		this.serverID = id;
+	}
+	
+	public int getID() {
+		return this.serverID;
+	}	
+	
+	
+}
